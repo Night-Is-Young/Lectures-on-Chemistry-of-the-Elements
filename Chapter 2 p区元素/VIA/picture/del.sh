1e51f8b187323a73530a3828fcc2dd1709773630
Night-Is-Young <trackers@tutamail.com>
2025-06-28 14:52:26 +0800

    submission

--- Chapter 2 p区元素/VIA/picture/del.sh
+++ Chapter 2 p区元素/VIA/picture/del.sh
@@ -2,5 +2,6 @@
 
 # 定位到脚本所在目录（即使从其他目录调用脚本）
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+for f in *.png; do [ -f "$f" ] && bmeps -c "$f" "${f%.png}.eps"; done
 find "$script_dir" -type f -name "*.png" -exec rm -f {} +
 find "$script_dir" -type f -name "*.pdf" -exec rm -f {} +
